eb1ee2c47a1d1ef7a36d73f3b4f917d3c9d09ddc
Conor Steel <csteel1@ithaca.edu>
2018-01-30 17:11:22 -0500

    Impl handles up to two input values
    Input string is split by commas and then each partition is checked for non-int characters.
    Valid numbers are added to a sum variable and returned upon completion.

--- src/edu/ithaca/steel/StringCalculatorImpl.java
+++ src/edu/ithaca/steel/StringCalculatorImpl.java
@@ -4,21 +4,22 @@ public class StringCalculatorImpl implements StringCalculator{
     @Override
     public int add(String numbers) {
         if(numbers.isEmpty())return 0;
-
-
-        // splits string into char array and iteratively checks for non-integers
-        char[] strSearch = numbers.toCharArray();
-        for(int i = 0; i < strSearch.length; i++){
-            // if current character not a digit and pattern does not suggest a negative digit
-            if((!Character.isDigit(strSearch[i])) && !((i==0)&&(strSearch[i]=='-')&&(strSearch.length>1))){
-                System.out.println("Parameters must be integer values");
-                return 0;
+        // split the string into array
+        String[] numList = numbers.split("\\s*,\\s*");
+        int sum = 0;
+        for(int j = 0; j < numList.length; j++) {
+            // splits string into char array and iteratively checks for non-integers
+            char[] strSearch = numList[j].toCharArray();
+            for (int i = 0; i < strSearch.length; i++) {
+                // if current character not a digit and pattern does not suggest a negative digit
+                if ((!Character.isDigit(strSearch[i])) && !((i == 0) && (strSearch[i] == '-') && (strSearch.length > 1))) {
+                    System.out.println("Parameters must be integer values");
+                    return 0;
+                }
             }
+            // increment sum by values that pass the test
+            sum += Integer.parseInt(numList[j]);
         }
-
-
-        int myVal = Integer.parseInt(numbers);
-
-        return myVal;
+        return sum;
     }
 }
