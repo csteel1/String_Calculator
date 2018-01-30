22d9b4429957aab4a5802e73e18d2a4bb0114ebc
Conor Steel <csteel1@ithaca.edu>
2018-01-30 16:46:49 -0500

    Tests for 2 number inputs
    Input error handling covered by single input tests.
    Equivalence partitions for this test are two positive values, two negative values, and a positive and negative value.

--- test/edu/ithaca/steel/StringCalculatorImplTest.java
+++ test/edu/ithaca/steel/StringCalculatorImplTest.java
@@ -13,12 +13,17 @@ class StringCalculatorImplTest {
         StringCalculator calculator = new StringCalculatorImpl();
         assertEquals(0,calculator.add(""));
 
-        // tests for single number strings;
+        // tests for single number strings
         assertEquals(7,calculator.add("7"));
         assertEquals(0,calculator.add("3.14"));
         assertEquals(0,calculator.add("0"));
         assertEquals(-23,calculator.add("-23"));
         assertEquals(0,calculator.add("b"));
+
+        // tests for two number strings
+        assertEquals(18,calculator.add("14,4"));
+        assertEquals(-10,calculator.add("-6,-4"));
+        assertEquals(13,calculator.add("-20,33"));
 
     }
 }
